e80c9fd1b445600737ee69682ce89fdfd467cf1c
m-marquez-medina21 <m.marquez.medina21@gmail.com>
2021-08-17 20:40:46 -0400

    commit

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,9 @@
+module ApplicationHelper
+    def current_user
+        User.find(session[:user_id]) unless session[:user_id].nil?
+    end
+
+    def logged?
+        session[:user_id].present?
+    end
+end
